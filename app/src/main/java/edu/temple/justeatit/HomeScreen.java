c5c9dd0305205c47d7ecdb4e0927e8c7621b7a57
1harryho <tuf52767@temple.edu>
2017-11-26 01:10:35 -0500

    Added camera code

--- app/src/main/java/edu/temple/justeatit/HomeScreen.java
+++ app/src/main/java/edu/temple/justeatit/HomeScreen.java
@@ -2,6 +2,11 @@ package edu.temple.justeatit;
 
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -11,10 +16,17 @@ import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class HomeScreen extends AppCompatActivity {
 
     RelativeLayout main;
     ImageButton imgButton;
+    static final int CAMERA_REQUEST_CODE = 1;
+    String picturePath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +38,7 @@ public class HomeScreen extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Toast.makeText(HomeScreen.this, "You clicked the camera!", Toast.LENGTH_SHORT).show();
+                startCamera();
             }
         });
     }
@@ -40,14 +53,47 @@ public class HomeScreen extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         if (itemId == R.id.action_Options) {
-            Toast.makeText(HomeScreen.this, "You clicked te options!", Toast.LENGTH_SHORT).show();
             openOptions();
         }
         return true;
     }
 
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK) {
+//            Bundle img = data.getExtras();
+//            Bitmap imgBitmap = (Bitmap) img.get("data");
+//        }
+//    }
+
     private void openOptions() {
         Intent intent = new Intent(this, OptionsActivity.class);
         startActivity(intent);
+    }
+
+    private void startCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (cameraIntent.resolveActivity(getPackageManager()) != null) {
+            File image = null;
+            try {
+                image = createImage();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (image != null) {
+                Uri photoURI = FileProvider.getUriForFile(this, "edu.temple.fileprovider", image);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE);
+            }
+        }
+    }
+
+    private File createImage() throws IOException {
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + time + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        picturePath = image.getAbsolutePath();
+        return image;
     }
 }
