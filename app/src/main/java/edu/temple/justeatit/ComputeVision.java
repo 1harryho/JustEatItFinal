e99fb4be9ce819aab1c1b10025331787abe3533b
Harry Ho <tuf52767@temple.edu>
2017-12-05 19:52:35 -0500

    Computer vision API is working, need to make it work for bitmaps

--- app/src/main/java/edu/temple/justeatit/ComputeVision.java
+++ app/src/main/java/edu/temple/justeatit/ComputeVision.java
@@ -6,6 +6,14 @@ import android.os.AsyncTask;
 import android.util.Base64;
 import android.util.Log;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -23,6 +31,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
@@ -37,57 +47,55 @@ public class ComputeVision extends AsyncTask<Bitmap, Void, JSONObject> {
 
     @Override
     protected JSONObject doInBackground(Bitmap... bitmaps) {
-        HttpURLConnection httpclient = null;
+        HttpClient client = new DefaultHttpClient();
+//        HttpURLConnection httpclient = null;
         try {
-            Uri.Builder builder = new Uri.Builder();
-            builder.scheme("https")
-                    .authority("westcentralus.api.cognitive.microsoft.com")
-                    .appendPath("vision")
-                    .appendPath("v1.0")
-                    .appendPath("analyze")
-                    .appendQueryParameter("visualFeatures", "Tags")
-                    .appendQueryParameter("language", "en");
+            Log.i("ComputeVision", "Creating URI");
+            URIBuilder builder = new URIBuilder(uriBase);
+            builder.setParameter("visualFeatures","Tags");
+            builder.setParameter("language","en");
+            URI uri = builder.build();
 
-            Log.i("ComputeVision", "Appended queries");
+            Log.i("ComputeVision", "Finished creating URI");
 
-            URL url = new URL(builder.build().toString());
-            httpclient = (HttpURLConnection) url.openConnection();
+            HttpPost REQUEST = new HttpPost(uri);
 
+            Log.i("ComputeVision", "Creating POST Request with uri");
 
+//            REQUEST.setHeader("Content-Type","multipart/form-data");
+            REQUEST.setHeader("Content-Type","application/json");
+            REQUEST.setHeader("Ocp-Apim-Subscription-Key",subscriptionKey);
 
-            httpclient.setRequestMethod("POST");
-            httpclient.setRequestProperty("Content-Type", "multipart/form-data");
-            httpclient.setRequestProperty("Ocp-Apim-Subscription-Key", subscriptionKey);
-            httpclient.setDoOutput(true);
-            httpclient.setDoInput(true);
-            httpclient.setUseCaches(false);
+            Log.i("ComputeVision", "Set request headers");
 
-            OutputStream out = new BufferedOutputStream(httpclient.getOutputStream());
-            BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(out, "UTF-8"));
+//            Bitmap bitmap = bitmaps[0];
+//            ByteArrayOutputStream stream = new ByteArrayOutputStream();
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
+//            byte[] byteArray = stream.toByteArray();
+//            String s = new String(byteArray);
+//            String s = Base64.encodeToString(byteArray, Base64.DEFAULT);
+//            StringEntity entity = new StringEntity(s);
+            StringEntity entity = new StringEntity("{\"url\":\"https://www.organicfacts.net/wp-content/uploads/2013/05/Banana3.jpg\"}");
+            REQUEST.setEntity(entity);
 
-            Log.i("ComputeVision", "Got writing streams");
+            Log.i("ComputeVision", "Set request body");
 
-            Bitmap bitmap = bitmaps[0];
-            ByteArrayOutputStream stream = new ByteArrayOutputStream();
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-            byte[] byteArray = stream.toByteArray();
-            String s = Base64.encodeToString(byteArray, Base64.DEFAULT);
+            HttpResponse response = client.execute(REQUEST);
+            HttpEntity responseEntity = response.getEntity();
 
-            writer.write(s);
+            if (responseEntity != null) {
+                Log.i("Entity", "Entity isn't null!");
+                String jsonString = EntityUtils.toString(responseEntity);
+                System.out.println(jsonString);
+            } else {
+                Log.i("Entity", "Entity is null");
+            }
+//
+//            Log.i("ComputeVision", "Connected");
+//
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(httpclient.getInputStream()));
 
-            writer.flush();
-
-            writer.close();
-
-            out.close();
-
-            httpclient.connect();
-
-            Log.i("ComputeVision", "Connected");
-
-            BufferedReader reader = new BufferedReader(new InputStreamReader(httpclient.getInputStream()));
-
-        } catch (IOException e) {
+        } catch (URISyntaxException | IOException e) {
             Log.e("ComputeVision", e.toString());
         }
         return null;
